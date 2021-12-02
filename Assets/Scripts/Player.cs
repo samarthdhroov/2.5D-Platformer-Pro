d2f8a85c57f569b4d39b7c3d5442e956fa7f6fbe
Samarth Dhroov <samarth.dhroov@hotmail.com>
2021-12-02 14:30:34 +0530

    Physics based controller and Double Jump in place. Working Fine.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _characterController;
+
+    private int _speed = 5;
+
+    private float _gravity = 1.5f;
+
+    private int _jumpHeight = 50;
+
+    private float _CacheYVelocity;
+
+    private bool _jumped;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        float _horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 _direction = new Vector3(_horizontalInput, 0, 0);
+
+        Vector3 _velocity = _direction * _speed;
+
+
+        if (_characterController.isGrounded == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _jumped = true;
+                _CacheYVelocity = _jumpHeight;
+            }
+            
+        }
+        else
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (_jumped == true)
+                {
+                    _CacheYVelocity += _jumpHeight + 1;
+                    _jumped = false;
+                }
+            }
+
+            _CacheYVelocity -= _gravity;
+        }
+
+        _velocity.y = _CacheYVelocity;
+
+        _characterController.Move(_velocity * Time.deltaTime);
+       
+        
+    }
+}
