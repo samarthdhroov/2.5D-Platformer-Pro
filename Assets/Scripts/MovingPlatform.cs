5aadf34330c8bd63355a6d96c60a25534a1e4c16
Samarth Dhroov <samarth.dhroov@hotmail.com>
2021-12-03 12:51:59 +0530

    Moving platform in place.Working Fine.

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _targetA, _targetB;
+
+    private float _speed = 2.0f;
+
+    private bool _targetReached = false;
+    
+
+    void FixedUpdate()
+    {
+        if(_targetReached == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetB.position, _speed * Time.deltaTime);
+        }
+        else if(_targetReached == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetA.position, _speed * Time.deltaTime);
+        }
+
+
+        if (transform.position == _targetB.position)
+        {
+            _targetReached = true;
+        }
+            
+       
+       if(transform.position == _targetA.position)
+        {
+            _targetReached = false;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = this.transform;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+}
